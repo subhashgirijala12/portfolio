8cb3eb220ebaebea0399703b69b72d257e490ddd
Deepak <subhashgirijala12@gmail.com>
2026-05-19 17:15:20 +0530

    Add AuthCallback route to App component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import './App.css';
+import AuthCallback from "./AuthCallback";
+
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+            <Route path="/auth/callback" element={<AuthCallback />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
